6b6d4f77a4d2bcf777c561bc57937ecca8b12b65
agil-cardo <ale.gcardona26@gmail.com>
2025-05-30 13:01:53 +0200

    test formulaire de contact

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Formulaire de Contact', () => {
+  beforeEach(() => {
+    cy.visit('https://agil-cardo.github.io/AGil-Portfolio/')
+    cy.get('[data-cy="contact-name"]', { timeout: 5000 }).should('be.visible');
   })
 })
+
+it('Vérifier le titre de la page', () => {
+  cy.get('[data-cy="contact-name"]').type('Pepito');
+  cy.get('[data-cy="contact-email"]').type('pepito@aol.com');
+  cy.get('[data-cy="contact-message"]').type('Ceci est un message de test');
+  cy.get('[data-cy="contact-send-btn"]').click();
+});
